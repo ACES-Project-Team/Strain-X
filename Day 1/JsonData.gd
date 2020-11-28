b6125c2effc89f1389a5d7bd7ba0da6c00fce7bb
casp3r2 <73689043+casp3r2@users.noreply.github.com>
2020-11-28 12:14:10 +0800

    fix item

--- Day 1/JsonData.gd
+++ Day 1/JsonData.gd
@@ -2,5 +2,14 @@ extends Node
 
 var item_data: Dictionary
 
-func _ready(): 
-	item_data = LoadData("
+func ready(): 
+	item_data = LoadData("res://Data/ItemData.json")
+
+func LoadData(file_path):
+	var json_Data
+	var file_data = File.new()
+	
+	file_data.open(file_path, File.READ)
+	json_Data = JSON.parse(file_data.get_as_text())
+	file_data.close()
+	return json_Data
