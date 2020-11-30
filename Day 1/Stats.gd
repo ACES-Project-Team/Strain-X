a2ee41b1b8bb2febfd3340dff5cbbc08fea14da1
casp3r2 <73689043+casp3r2@users.noreply.github.com>
2020-11-30 21:00:28 +0800

    Enemy JM

---
+++ Day 1/Stats.gd
@@ -0,0 +1,20 @@
+extends Node
+
+export var MAX_HEALTH = 1 
+var HEALTH = MAX_HEALTH
+signal no_health 
+signal health_changed(value)
+signal max_health_changed(value)
+func set_max_health(value):
+	MAX_HEALTH = value 
+	self.HEALTH = min(HEALTH,MAX_HEALTH)	
+	emit_signal("max_health_changed",MAX_HEALTH)
+
+func set_health(value):
+	HEALTH = value
+	emit_signal("health_changed",HEALTH)
+	if HEALTH <= 0:
+		emit_signal("no_health")
+
+func _ready():
+	self.HEALTH = MAX_HEALTH
