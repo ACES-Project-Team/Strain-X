636b69fe7e41b6b1a0c60ce08f19326206f8345f
casp3r2 <73689043+casp3r2@users.noreply.github.com>
2020-12-08 18:07:44 +0800

    Carnage phase 1

--- Day 1/Carnagephase1.gd
+++ Day 1/Carnagephase1.gd
@@ -1,12 +1,37 @@
 extends KinematicBody2D
 
 const EnemyDeathEffect = preload("res://Assets/Effects/EnemyDeathEffect.png")
-
+export var ACCELERATION = 300 
+export var MAX_SPEED = 50
+export var FRICTION = 200
+enum {
+	IDLE,
+	WANDER,
+	CHASE
+}
+var velocity = Vector2.ZERO
 var knockback = Vector2.ZERO 
 
+var state = CHASE 
+
+onready var stats = $Stats
+
 func _physics_process(delta):
-	knockback = knockback.move_toward(Vector2.ZERO, 200 * delta)
+	knockback = knockback.move_toward(Vector2.ZERO, FRICTION * delta)
 	knockback = move_and_slide(knockback)
+	
+	match state:
+		IDLE:
+			velocity = velocity.move_toward(Vector2.ZERO, FRICTION * delta)
+		
+		WANDER:
+			pass
+		
+		CHASE:
+			pass 
+
+func seek_player():
+	pass
 
 func _on_Hurtbox_area_entered(area):
 	knockback = Vector2.RIGHT * 120
