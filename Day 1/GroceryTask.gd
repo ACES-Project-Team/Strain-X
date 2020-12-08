d15a01c37df1d937a80b0c9f7c25a85fe86a2197
dreadulin <liannaahdulin@gmail.com>
2020-12-09 01:38:20 +0800

    Add Tasks on Traffic Cones and Grocery Stall

--- Day 1/GroceryTask.gd
+++ Day 1/GroceryTask.gd
@@ -4,6 +4,13 @@ var can_interact = false
 func _ready():
 	pass
 
+		
+func _on_Area2D_body_entered(body):
+	if body.name == "GroceryOutfit ":
+		$Label.show()
+		can_interact = true
+
+
 func _on_Area2D_body_exited(body):
 	if body.name == "GroceryOutfit ":
 		$Label.hide()
@@ -12,11 +19,4 @@ func _on_Area2D_body_exited(body):
 
 func _input(event):
 	if Input.is_action_just_pressed("PickUp") and can_interact == true: 
-		$Label.text = "Go to Grocery" 
-
-
-
-func _on_Area2D_body_entered(body):
-	if body.name == "GroceryOutfit ":
-		$Label.show()
-		can_interact = true
+		$Label.text = "Get Foods" 
