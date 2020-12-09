8f155659d6b27602501d0f3e5778b0c2b1e49564
dreadulin <liannaahdulin@gmail.com>
2020-12-09 13:34:41 +0800

    Fix Task System

---
+++ Day 1/Task2.gd
@@ -0,0 +1,24 @@
+extends Area2D
+
+var can_interact = false
+
+func _ready():
+	pass 
+		
+func _input(event):
+	if Input.is_action_just_pressed("PickUp") and can_interact == true: 
+		$Label.text = "Avoid Karens & Save Neighbor to Earn Reward"
+		
+
+func _on_Task2_body_entered(body):
+	if body.name == "Player":
+		$Label.show()
+		can_interact = true
+	
+
+
+func _on_Task2_body_exited(body):
+	if body.name == "Player":
+		$Label.hide()
+		can_interact = false 
+		$Label.text = "Interact (E)"
