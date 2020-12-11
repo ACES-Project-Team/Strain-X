134e3d62706fd71ce5cd902eb142a6291426673c
UnityGrave <72306255+UnityGrave@users.noreply.github.com>
2020-12-11 17:03:23 +0800

    Fix glitch in cutscene and make the cameras more smooth

--- Day 1/MCHouse.gd
+++ Day 1/MCHouse.gd
@@ -4,6 +4,6 @@ onready var cam = $CutsceneCamera
 
 func _ready():
 	yield(cam.move_to_position(cam.global_position + Vector2.LEFT * 300, 50), "completed")
-	yield(get_tree().create_timer(2.0), "timeout")
-	yield(cam.return_to_old_position(250), "completed")
+	yield(get_tree().create_timer(1.0), "timeout")
+	yield(cam.return_to_old_position(500), "completed")
 	
