b5a8cd07b8ed11dd003b46227dd1d3c7ddeb2873
dreadulin <liannaahdulin@gmail.com>
2020-12-12 11:41:37 +0800

    Fix Bugs

--- Day 1/One.gd
+++ Day 1/One.gd
@@ -1,23 +1,27 @@
 extends Area2D
 
 var can_interact = false
-
 func _ready():
-	pass 
-		
+	pass # Replace with function body.
+	
+
+
+	
 func _input(event):
 	if Input.is_action_just_pressed("PickUp") and can_interact == true: 
-		$Label.text = "Defeat More Volatiles & Avoid Karens"
+		$Label.text = "Defeat Volatiles and Avoid Karens"
 		
 
-func _on_Area2D_body_entered(body):
+
+func _on_One_body_entered(body):
 	if body.name == "SurgicalMaskOutfit ":
 		$Label.show()
 		can_interact = true
-	
 
-func _on_Area2D_body_exited(body):
+
+func _on_One_body_exited(body):
 	if body.name == "SurgicalMaskOutfit ":
 		$Label.hide()
 		can_interact = false 
 		$Label.text = "Interact (E)"
+		
